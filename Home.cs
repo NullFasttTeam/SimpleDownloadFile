26fa55a602da3d515f316142741d2447189b278d
HugoFasttDEV <86870433+HugoFastDEV@users.noreply.github.com>
2022-07-04 09:45:01 +0200

    Version 1

---
+++ Home.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleDownloadFile
+{
+    public class Home
+    {
+        public static void DownloadFile(string link, string lugar, string name)
+        {
+            try {
+                Uri myUri = new Uri(link);
+            WebClient webClient = new WebClient();
+           webClient.DownloadFileAsync(myUri, lugar, name);
+            } catch {
+                MessageBox.Show("Error al descargar el archivo", "SimpleDownloadFile");
+            }
+           
+        }
+        public static void DownloadString(string web)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadString(web);
+        }
+    }
+}
